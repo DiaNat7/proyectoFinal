e5067d8ea0e6ca2fc631b7e2650c684fc407a478
Diana <diana.ramirez0783@gmail.com>
2026-05-25 20:23:06 -0600

    Corrección de permisos en favoritos

--- Back/src/routes/favoritos.routes.js
+++ Back/src/routes/favoritos.routes.js
@@ -7,8 +7,8 @@ const { verificarToken, verificarAdmin } = require('../middlewares/auth.middlewa
 router.get('/', verificarToken, async (req, res, next) => { try { res.json(await svc.getAll()); } catch(e){ next(e) }});
 router.get('/:id', verificarToken, async (req, res, next) => { try { res.json(await svc.getById(req.params.id)); } catch(e){ next(e) }});
 // Solo admin puede crear, editar, eliminar
-router.post('/',      verificarToken, verificarAdmin, async (req, res, next) => { try { res.status(201).json(await svc.create(req.body));            } catch(e){ next(e) }});
-router.put('/:id',    verificarToken, verificarAdmin, async (req, res, next) => { try { res.json(await svc.update(req.params.id, req.body));         } catch(e){ next(e) }});
-router.delete('/:id', verificarToken, verificarAdmin, async (req, res, next) => { try { res.json(await svc.remove(req.params.id));                   } catch(e){ next(e) }});
+router.post('/',      verificarToken,  async (req, res, next) => { try { res.status(201).json(await svc.create(req.body));            } catch(e){ next(e) }});
+router.put('/:id',    verificarToken,  async (req, res, next) => { try { res.json(await svc.update(req.params.id, req.body));         } catch(e){ next(e) }});
+router.delete('/:id', verificarToken,  async (req, res, next) => { try { res.json(await svc.remove(req.params.id));                   } catch(e){ next(e) }});
 
 module.exports = router;
